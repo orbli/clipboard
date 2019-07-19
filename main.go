c60b83146c8bc59571c5a19ecce1d4f36192a63c
orb li <mail@orbb.li>
2019-07-20 04:51:33 +0800

    Use google memcache

--- main.go
+++ main.go
@@ -1,74 +1,76 @@
 package main
 
 import (
-	"fmt"
-	"github.com/patrickmn/go-cache"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"time"
+        "context"
+        "fmt"
+        "google.golang.org/appengine"
+        "google.golang.org/appengine/memcache"
+        "io/ioutil"
+        "log"
+        "net/http"
 )
 
 type (
-	Handler struct{}
-)
-
-var (
-	c *cache.Cache
+        Handler struct{}
 )
 
 func main() {
-	c = cache.New(24*60*time.Minute, 48*60*time.Minute)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-	log.Printf("Listening on port %s", port)
-	httpServer := http.Server{
-		Addr:    fmt.Sprintf(":%s", port),
-		Handler: Handler{},
-	}
-	log.Fatal(httpServer.ListenAndServe())
+        http.HandleFunc("/", Handler{}.ServeHTTP)
+        appengine.Main()
 }
 func (Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	body := BodyString(r)
-	log.Printf("Method: %s , URI: %s, Body: %s", r.Method, r.URL.Path, body)
-	switch r.Method {
-	case "GET":
-		if r.URL.Path != "/" {
-			fmt.Fprint(w, GetValue(c, r.URL.Path))
-		} else {
-			b, _ := ioutil.ReadFile("index.html")
-			fmt.Fprint(w, string(b))
-		}
-	case "POST":
-		if r.URL.Path != "/" {
-			c.Set(r.URL.Path, body, cache.DefaultExpiration)
-			fmt.Fprint(w, GetValue(c, r.URL.Path))
-		}
-	case "DELETE":
-		if r.URL.Path != "/" {
-			c.Set(r.URL.Path, "", cache.DefaultExpiration)
-			fmt.Fprint(w, GetValue(c, r.URL.Path))
-		}
-	default:
-		http.NotFound(w, r)
-	}
-	return
+        ctx := appengine.NewContext(r)
+        bodybytes, err := ioutil.ReadAll(r.Body)
+        if err != nil {
+                fmt.Fprint(w, err.Error())
+        }
+        body := string(bodybytes)
+        log.Printf("Body: %s", body)
+        switch r.Method {
+        case "GET":
+                if r.URL.Path == "/" {
+                        b, _ := ioutil.ReadFile("index.html")
+                        fmt.Fprint(w, string(b))
+                        return
+                } else {
+                        fmt.Fprint(w, GetValue(ctx, r.URL.Path))
+                }
+        case "POST":
+                if r.URL.Path != "/" {
+                        fmt.Fprint(w, SetValue(ctx, r.URL.Path, body))
+                }
+        case "DELETE":
+                if r.URL.Path != "/" {
+                        if err := memcache.Delete(ctx, r.URL.Path); err != nil {
+                                fmt.Fprint(w, err.Error())
+                        }
+                }
+        default:
+                http.NotFound(w, r)
+                return
+        }
+        w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+        w.Header().Set("Pragma", "no-cache")
+        w.Header().Set("Expires", "0")
+        return
 }
-func GetValue(c *cache.Cache, key string) string {
-	if value, found := c.Get(key); found {
-		return value.(string)
-	} else {
-		return ""
-	}
+func GetValue(ctx context.Context, key string) string {
+        if item, err := memcache.Get(ctx, key); err == memcache.ErrCacheMiss {
+                return "Error cache miss"
+        } else if err != nil {
+                return err.Error()
+        } else {
+                return string(item.Value)
+        }
 }
-func BodyString(r *http.Request) string {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		panic(err)
-	}
-	return string(body)
+func SetValue(ctx context.Context, key, value string) string {
+        item := &memcache.Item{
+                Key:   key,
+                Value: []byte(value),
+        }
+        if err := memcache.Set(ctx, item); err != nil {
+                return err.Error()
+        } else {
+                return GetValue(ctx, key)
+        }
 }
