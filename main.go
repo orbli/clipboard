4c8d5d1560ae75d61e06bb972dd539a3ead8f34a
orbli.home.linuxsub <mail@orbb.li>
2019-07-20 08:26:29 +0800

    Use redis

--- main.go
+++ main.go
@@ -1,76 +1,93 @@
 package main
 
 import (
-        "context"
-        "fmt"
-        "google.golang.org/appengine"
-        "google.golang.org/appengine/memcache"
-        "io/ioutil"
-        "log"
-        "net/http"
+	"fmt"
+	"github.com/go-redis/redis"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 type (
-        Handler struct{}
+	Handler struct{}
+)
+
+var (
+	cache *redis.Client
 )
 
 func main() {
-        http.HandleFunc("/", Handler{}.ServeHTTP)
-        appengine.Main()
+	cache = redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PSWD"),
+		DB:       0,
+	})
+	if _, err := cache.Ping().Result(); err != nil {
+		log.Fatal(err)
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	httpServer := http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: Handler{},
+	}
+	log.Println("Serve")
+	log.Fatal(httpServer.ListenAndServe())
 }
 func (Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-        ctx := appengine.NewContext(r)
-        bodybytes, err := ioutil.ReadAll(r.Body)
-        if err != nil {
-                fmt.Fprint(w, err.Error())
-        }
-        body := string(bodybytes)
-        log.Printf("Body: %s", body)
-        switch r.Method {
-        case "GET":
-                if r.URL.Path == "/" {
-                        b, _ := ioutil.ReadFile("index.html")
-                        fmt.Fprint(w, string(b))
-                        return
-                } else {
-                        fmt.Fprint(w, GetValue(ctx, r.URL.Path))
-                }
-        case "POST":
-                if r.URL.Path != "/" {
-                        fmt.Fprint(w, SetValue(ctx, r.URL.Path, body))
-                }
-        case "DELETE":
-                if r.URL.Path != "/" {
-                        if err := memcache.Delete(ctx, r.URL.Path); err != nil {
-                                fmt.Fprint(w, err.Error())
-                        }
-                }
-        default:
-                http.NotFound(w, r)
-                return
-        }
-        w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-        w.Header().Set("Pragma", "no-cache")
-        w.Header().Set("Expires", "0")
-        return
+	bodybytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+	}
+	body := string(bodybytes)
+	log.Printf("Body: %s", body)
+	switch r.Method {
+	case "GET":
+		if r.URL.Path == "/" {
+			b, _ := ioutil.ReadFile("index.html")
+			fmt.Fprint(w, string(b))
+			return
+		} else {
+			fmt.Fprint(w, GetValue(r.URL.Path))
+		}
+	case "POST":
+		if r.URL.Path != "/" {
+			fmt.Fprint(w, SetValue(r.URL.Path, body))
+		}
+	case "DELETE":
+		if r.URL.Path != "/" {
+			fmt.Fprint(w, DelValue(r.URL.Path))
+		}
+	default:
+		http.NotFound(w, r)
+		return
+	}
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
+	return
 }
-func GetValue(ctx context.Context, key string) string {
-        if item, err := memcache.Get(ctx, key); err == memcache.ErrCacheMiss {
-                return "Error cache miss"
-        } else if err != nil {
-                return err.Error()
-        } else {
-                return string(item.Value)
-        }
+func GetValue(key string) string {
+	if val, err := cache.Get(key).Result(); err != nil {
+		return err.Error()
+	} else {
+		return fmt.Sprintf("%v", val)
+	}
 }
-func SetValue(ctx context.Context, key, value string) string {
-        item := &memcache.Item{
-                Key:   key,
-                Value: []byte(value),
-        }
-        if err := memcache.Set(ctx, item); err != nil {
-                return err.Error()
-        } else {
-                return GetValue(ctx, key)
-        }
+func SetValue(key, value string) string {
+	if err := cache.Set(key, value, 0).Err(); err != nil {
+		return err.Error()
+	} else {
+		return GetValue(key)
+	}
+}
+func DelValue(key string) string {
+	if err := cache.Del(key).Err(); err != nil {
+		return err.Error()
+	} else {
+		return GetValue(key)
+	}
 }
